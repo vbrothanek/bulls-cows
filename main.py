fca4050d760c6d21cd39519088fed17e305670b9
Vojtech Brothanek <vbrothanek@brotel.cz>
2021-12-30 16:09:12 +0100

    Main python script for running the program

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
 from functions import *
 
 
-
 def main():
     print(welcome())
     level = game_level()
+    print(SIMPLE_DIVIDER)
     secret_number = generated_number(level)
     # print(f"DEBUG GENERATED NUMBER {secret_number}")
     count_asking = 0
 
+    secret_number_loop = len(secret_number)
+
     while True:
-        user_guess = input(f"Prosim zadejte vas tip \n")
+
+        user_guess = ""
+
+        while secret_number_loop != len(user_guess):
+            user_guess = input(f"Prosim zadejte vas tip \n")
+            # print(len(user_guess), ",", secret_number_loop)
+            if len(user_guess) < secret_number_loop:
+                print("Zadali jste prilis malo cisel.. Zadejte Vas tip znovu", SIMPLE_DIVIDER, sep="\n")
+            elif len(user_guess) > secret_number_loop:
+                print("Zadali jste prilis mnoho cisel.. Zadejte Vas tip znovu", SIMPLE_DIVIDER, sep="\n")
+
         count_asking += 1
         comparsion = number_comparsion(user_guess, secret_number)
 
         if comparsion == {
             "Bulls": level,
             "Cows": 0
-            }:
+        }:
             print(DUAL_DIVIDER)
             print(f"Dobra hra! Zvladnul jsi to na {count_asking} pokus :-)")
             print(DUAL_DIVIDER)
             break
         else:
+            print(SIMPLE_DIVIDER)
             print(f"Bulls: {comparsion['Bulls']} & Cows: {comparsion['Cows']}")
-
-
-
-
+            print(SIMPLE_DIVIDER)
 
 if __name__ == "__main__":
     main()
