bd650bb6989ff4e9a263607e060a62bee6f5b287
Vojtech Brothanek <vbrothanek@brotel.cz>
2021-12-30 14:04:00 +0100

    In progress: while loop for polling user input

--- functions.py
+++ functions.py
@@ -33,13 +33,25 @@ def user_guess_input(user_guess):
         return user_input_str
 
 
-
 def number_comparsion(user_input_str, secret_number):
     if not isinstance(user_input_str, str) or not isinstance(secret_number, str):
         raise ValueError("Wrong input")
 
-    if len(user_input_str) != len(secret_number) or len(user_input_str) < 4:
-        raise ValueError("Too long or too short!")
+
+    while True:
+
+        try:
+            if len(user_input_str) == len(secret_number):
+                break
+        except:
+            # if len(user_input_str) != len(secret_number) or len(user_input_str) < 4:
+            #     raise ValueError("Zadali jste prilis mnoho nebo malo cisel.. Zadejte Vas tip znovu")
+            continue
+
+            # if len(user_input_str) != len(secret_number) or len(user_input_str) < 4:
+            #     print(f"Zadali jste prilis mnoho nebo malo cisel.. Zadejte Vas tip znovu")
+
+            # raise ValueError("Too long or too short!")
 
     bulls_count = 0
     cows_count = 0
